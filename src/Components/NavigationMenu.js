0b298fa53927e672895fed74b6abc4ebb4d491d3
eric vara <vara.ada91@gmail.com>
2024-08-13 20:27:30 -0500

    rearranges Navigation menu styles

--- src/Components/NavigationMenu.js
+++ src/Components/NavigationMenu.js
@@ -1,42 +1,38 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { CartContext } from "../contexts/CartContext";
-import "./NavigationMenu.css";
+import styles from "./NavigationMenu.css";
 
 function NavigationMenu() {
   const { cart } = useContext(CartContext);
-
-  const totalItemsInCart = cart.reduce((total, item) => total + item.quantity, 0);
+  const totalItemsInCart = cart.reduce(
+    (total, item) => total + item.quantity,
+    0
+  );
 
   return (
-    <nav className="navigation-menu">
+    <nav className={styles.navigationMenu}>
       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/streamlist">Stream List</Link>
-        </li>
-        <li>
-          <Link to="/favorites">Favorites</Link> 
-        </li>
-        <li>
-          <Link to="/movies">Movies</Link>
-        </li>
-        <li>
-          <Link to="/subscriptions">Subscriptions</Link>        
-        </li>
-        <li>
-          <Link to="/cart">Cart ({totalItemsInCart})</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
+        <NavItem to="/" label="Home" />
+        <NavItem to="/streamlist" label="Stream List" />
+        <NavItem to="/favorites" label="Favorites" />
+        <NavItem to="/movies" label="Movies" />
+        <NavItem to="/subscriptions" label="Subscriptions" />
+        <NavItem to="/cart" label={`Cart (${totalItemsInCart})`} />
+        <NavItem to="/about" label="About" />
       </ul>
     </nav>
   );
 }
 
+function NavItem({ to, label }) {
+  return (
+    <li>
+      <Link to={to} className={styles.navLink}>
+        {label}
+      </Link>
+    </li>
+  );
+}
+
 export default NavigationMenu;
-
-
